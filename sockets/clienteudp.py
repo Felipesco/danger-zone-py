b39d0f7ba5f1a61fdf7fb60d544a74b33c5373ce
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-28 20:07:05 -0300

    Update clienteudp.py

--- sockets/clienteudp.py
+++ sockets/clienteudp.py
@@ -5,16 +5,9 @@ import socket
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
-    cliente.connect(("google.com", 80))
+    cliente.sendto("Teste de mensagem", ("google.com", 80))
 
-    st = 'GET /http/1.1\nHost: google.com\n\n'
-
-    byt = st.encode()
-
-    cliente.send(byt)
-
-    resp = str(cliente.recv(1024))
-    print(resp)
+    resp = cliente.recvfrom(1024)
 
 except:
     print("Ocorreu erro na conexão")
