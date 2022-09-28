78e5a760137e408ec3888e136199d183bc4d171a
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-28 20:19:29 -0300

    Update clienteudp.py

--- sockets/clienteudp.py
+++ sockets/clienteudp.py
@@ -7,7 +7,12 @@ cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     cliente.sendto("Teste de mensagem", ("127.0.0.1", 666))
 
-    resp = cliente.recvfrom(1024)
+    resp, userServer = cliente.recvfrom(1024)
 
-except:
+    print(str(userServer[0])+": "+ str(resp))
+
+    cliente.close()
+
+except Exception as erro:
     print("Ocorreu erro na conexão")
+    print(erro)
