1b45e380b57f7b60cec3bd080f9c9783d7a61ca9
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-28 20:45:23 -0300

    Create servidor.py

---
+++ sockets/servidor.py
@@ -0,0 +1,16 @@
+import socket 
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = "0.0.0.0"
+porta = 22
+
+try:
+    servidor.bind((ip, porta))
+    servidor.listen(5)
+    print("Escutando...\nIP: "+ip+":"+str(porta))
+
+    print(servidor.accept())
+
+except Exception as erro:
+    print("Algo deu erro")
+    print(erro)
